2e4909eab94dfe3be0b2e620751d2804a77fdcd8
David Andres Diaz Garcia <davidandresdiazgarcia@outlook.com>
2020-12-22 12:21:07 -0600

    updated

--- reportlogistica/models/models.py
+++ reportlogistica/models/models.py
@@ -14,19 +14,38 @@ class ReportCompra(models.Model):
 	dias_retraso = fields.Integer(string="Dias de retraso", store=True, compute="DiasRetraso")
 	dias_invent = fields.Float(string="Dias inventario", store=True, compute="DiasInventario")
 	cant_compr_confirm = fields.Integer(string="Cant compras confirmadas", store=True, compute="TotalComprasConfirm")
+	disponible_qty = fields.Float(string="Cantidad Disponible")
 	# pedido_compra = fields.Date()
+	
+	@api.multi
+	def TotalComprasConfirm(self):
+		self.cant_compr_confirm = 0
+		for rec in self:
+			product = self.env['product.product'].search([('product_tmpl_id','=',rec.id)])
+			purchase_order = self.env['purchase.order'].search([('product_id','=',product.id), ('state','=','purchase')], order='id desc')
+			for x in purchase_order:
+				for z in x.order_line:
+					if z.product_id.id == product.id:
+						rec.fecha_pedido_compra = x.date_order
+						rec.cant_compr_confirm += (z.product_qty - z.qty_received)
+		self.disponible_qty = self.qty_available + self.cant_compr_confirm
+	
 	@api.one
 	def ComputeReport(self):
 		product = self.env['product.product'].search([('product_tmpl_id','=',self.id)])
 		busquedad = self.env['purchase.order.line'].search([('product_id','=',product.id)], order='id desc', limit=1)
-		self.fecha_previs = busquedad.date_planned
-		# busquedad = self.env['stock.move'].search([('product_id','=',product.id)], order='id desc', limit=1)
-		# self.fecha_previs = busquedad.date
-		# self.fecha_pedido_compra = busquedad.purchase_line_id.order_id.date_order
-		# print('jjjjjjjjjjjjjjjjjjjjjjjjj', self.fecha_pedido_compra)
+		reci = 0
+		for rec in busquedad:
+			reci += rec.qty_received
+
+		if reci > 0:
+			self.fecha_previs = busquedad.date_planned
+		else:
+			self.fecha_previs = ''
 
 	@api.one
 	def DiasRetraso(self):
+		self.dias_retraso = 0
 		if self.fecha_previs:
 			fecha_prevista_str = str(self.fecha_previs)
 			fecha_prev = dateutil.parser.parse(fecha_prevista_str).date()
@@ -49,7 +68,8 @@ class ReportCompra(models.Model):
 			ano_actual = datetime.datetime.now().strftime('%Y')
 			fecha_actual = date(int(ano_actual),int(mes_actual),int(dia_actual))
 			dias = abs(fecha_actual - fecha_retrazo).days
-			self.dias_retraso = dias
+			if self.cant_compr_confirm > 0:
+				self.dias_retraso = dias
 			# print ("%s dias de diferencia entre %s y el %s" % (dias, fecha_retrazo, fecha_actual))
 			# print ("%s años de diferencia entre %s y el %s" % (int(dias/365), fecha_retrazo, fecha_actual))
 	def user_log(self):
@@ -59,7 +79,8 @@ class ReportCompra(models.Model):
 		return usuario
 
 	@api.multi
-	def DiasInventario(self, dias):
+	def DiasInventario(self):
+		dias = 60
 		self.dias_invent = 0
 		fecha_anterior = ""
 		fecha_actual = ""
@@ -70,38 +91,23 @@ class ReportCompra(models.Model):
 			fecha_actual = date(int(ano_actual),int(mes_actual),int(dia_actual))
 			fecha_anterior = date(int(ano_actual),int(mes_actual),int(dia_actual)) + timedelta(days=-dias)
 		if dias:
+			print(dias)
 			if dias > 0:
 				product = self.env['product.product'].search([('product_tmpl_id','=',self.id)])
 				move_line = self.env['stock.move.line'].search([('product_id','=', product.id)])
 				consumo_diario_total = 0
 				for rec in move_line:
 					rec.dias_invent = 0
-					print(rec.date.date(), rec.date.date(), fecha_anterior, 'ESTO', fecha_actual)
 					if rec.date.date() <= fecha_actual and rec.date.date() >= fecha_anterior:
+						# print(fecha_anterior, '------------' ,rec.date.date(),  '-------------', fecha_actual)
 						if rec.location_dest_id.check_location == True:
 							print(rec.qty_done)
 							consumo_diario_total += rec.qty_done
+				# print('aaaaaaaaaaaaaaaaaaa', consumo_diario_total, product.name)
 				consumo_diario = consumo_diario_total / dias
 				if consumo_diario > 0 and self.qty_available > 0: 
-					self.dias_invent = consumo_diario / self.qty_available
-					print('111111111111111111111111', self.dias_invent)
+					self.dias_invent =  self.qty_available / consumo_diario
 
-	@api.multi
-	def TotalComprasConfirm(self):
-		self.cant_compr_confirm = 0
-		for rec in self:
-			# print('EEEEEEO')
-			product = self.env['product.product'].search([('product_tmpl_id','=',rec.id)])
-			# print(product.name)
-			# stock_move = self.env['stock.move'].search([('product_id','=',product.id), ('picking_id.state','in',('confirmed','assigned')), ('picking_id.picking_type_code','=',"incoming")], order='id desc')
-			purchase_order = self.env['purchase.order'].search([('product_id','=',product.id), ('state','=','purchase')], order='id desc')
-
-			for x in purchase_order:
-				for z in x.order_line:
-					# print("EEEEEEEEEEOOOOOOOOOOOOO")
-					if z.product_id.id == product.id:
-						# print('OHHHHH RIGHT',z.product_qty)
-						rec.cant_compr_confirm += (z.product_qty - z.qty_received)
 
 class InheritPayment(models.Model):
 	_inherit = 'account.payment'
